d55908a73a953f69b289caeb26a2e74d8ffd9a92
nidhip1305 <157209136+nidhip1305@users.noreply.github.com>
2026-04-24 15:53:15 +0530

    Update Room.java

--- src/main/java/com/hostel/smart_hostel/model/Room.java
+++ src/main/java/com/hostel/smart_hostel/model/Room.java
@@ -26,8 +26,7 @@ public class Room {
     private String assignedStudents = "";
 
     // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() { return id; }public void setId(Long id) { this.id = id; }
     public String getRoomNumber() { return roomNumber; }
     public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber; }
     public int getFloor() { return floor; }
